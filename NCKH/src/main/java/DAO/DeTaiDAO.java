62f8160ba3df5df4b2582fd020bb196f042d3b2c
nquynhnhi0912 <nquynhnhi0912@gmail.com>
2023-12-12 01:14:16 +0700

    Danh sách đề tài giảng viên

---
+++ NCKH/src/main/java/DAO/DeTaiDAO.java
@@ -0,0 +1,48 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import java.util.List;
+import Models.DETAI;
+import Util.HandleException;
+import Util.JDBC;
+
+
+
+public class DeTaiDAO {
+	private static final String SELECT_ALL_DETAI = "SELECT * FROM nckh.detai";
+	
+	public DeTaiDAO() {}
+	
+	public List<DETAI> selectAllDeTais() {
+		// using try-with-resources to avoid closing resources (boiler plate code)
+        List <DETAI> detais = new ArrayList < > ();
+
+        // Step 1: Establishing a Connection
+        try (Connection connection = JDBC.getConnection();PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_DETAI);) {
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: Process the ResultSet object.
+            while (rs.next()) {
+                String MaDeTai1 = rs.getString("MaDeTai");
+                String TieuDe1 = rs.getString("TieuDe");
+                String MoTa1 = rs.getString("MoTa");
+                boolean TrangThai1 = rs.getBoolean("TrangThai");
+                int KinhPhi1 = rs.getInt("KinhPhi");
+                Date NDKTC1 = rs.getDate("NgayDKTC");
+                String MaNV1 = rs.getString("MaNV");
+                detais.add(new DETAI(MaDeTai1, TieuDe1, MoTa1, TrangThai1, KinhPhi1, NDKTC1, MaNV1));
+            }
+        } catch (SQLException exception) {
+        	HandleException.printSQLException(exception);
+        }
+        return detais;
+	}
+}
